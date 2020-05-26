04fb75b6757bd24152f25e158dbbacd2f3e8c91f
Nithyanantham <nithyananth1992@gmail.com>
2020-05-26 14:05:25 +0530

    Add Question2 scene

---
+++ DemoSql/Assets/Question2Request.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Question2Request
+{
+    public HeadachesTime HeadachesTime;
+    public HeadachesatNight HeadachesatNight;
+    public string PremonitorySymptoms;
+    public string MedicationsTaken;
+}
+public enum HeadachesTime
+{
+    Morning,
+    AfterNoon,
+    Evening,
+    NoPattern,
+    DuringNight
+}
+public enum HeadachesatNight
+{
+    Never,
+    Occaasionally,
+    Often
+}
---
+++ DemoSql/Assets/QuestionTwoScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class QuestionTwoScript : MonoBehaviour
+{
+    public GameObject formquestiontwo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void Submit()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
